835d00540c6e99bf0c3587d76e36def50b695342
melio <dhitanimam05@gmail.com>
2025-03-28 22:26:36 +0700

    feat: Category API Connected and Input Drop Zone Added

---
+++ src/components/ui/InputFile/InputFile.tsx
@@ -0,0 +1,95 @@
+import { cn } from "@/utils/cn";
+import Image from "next/image";
+import { ChangeEvent, useEffect, useId, useRef, useState } from "react";
+import { CiSaveUp2 } from "react-icons/ci";
+
+interface PropTypes {
+  className?: string;
+  isDropable?: boolean;
+  name: string;
+}
+
+const InputFile = (props: PropTypes) => {
+  const [uploadedImg, setUploadedImg] = useState<File | null>(null);
+  const { className, isDropable = false, name } = props;
+  const drop = useRef<HTMLLabelElement>(null);
+  const dropzoneId = useId();
+
+  const handleDragOver = (e: DragEvent) => {
+    if (isDropable) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  };
+
+  const handleDrop = (e: DragEvent) => {
+    e.preventDefault();
+    setUploadedImg(e.dataTransfer?.files?.[0] || null);
+  };
+
+  useEffect(() => {
+    const dropCurrent = drop.current;
+    if (dropCurrent) {
+      dropCurrent.addEventListener("dragover", handleDragOver);
+      dropCurrent.addEventListener("drop", handleDrop);
+      return () => {
+        dropCurrent.removeEventListener("dragover", handleDragOver);
+        dropCurrent.removeEventListener("drop", handleDrop);
+      };
+    }
+  }, []);
+
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const files = e.currentTarget.files;
+    if (files && files.length > 0) {
+      setUploadedImg(files[0]);
+    }
+  };
+
+  return (
+    <label
+      ref={drop}
+      htmlFor={`dropzone-file-${dropzoneId}`}
+      className={cn(
+        "flex min-h-24 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100",
+        className,
+      )}
+    >
+      {uploadedImg ? (
+        <div className="flex flex-col items-center justify-center p-5">
+          <div className="mb-2 w-1/2">
+            <Image
+              src={URL.createObjectURL(uploadedImg)}
+              alt="image"
+              className="!relative"
+            />
+          </div>
+          <p className="text-center text-sm font-semibold text-gray-500">
+            {uploadedImg.name}
+          </p>
+        </div>
+      ) : (
+        <div className="flex flex-col items-center justify-center p-5">
+          <div className="mb-2 w-1/2">
+            <CiSaveUp2 className="mb-2 h-10 w-10 text-gray-400" />
+          </div>
+          <p className="text-center text-sm font-semibold text-gray-500">
+            {isDropable
+              ? "Drag and drop or click to upload file here"
+              : "Click to upload file"}
+          </p>
+        </div>
+      )}
+      <input
+        name={name}
+        type="file"
+        className="hidden"
+        accept="image/*"
+        id={`dropzone-file-${dropzoneId}`}
+        onChange={handleOnChange}
+      />
+    </label>
+  );
+};
+
+export default InputFile;
