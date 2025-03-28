213787728e25656831dcce6ac48fafa67d35f2fc
melio <dhitanimam05@gmail.com>
2025-03-28 11:06:07 +0700

    feat: Data Table UI for Admin Category Page

---
+++ src/pages/admin/category/index.tsx
@@ -0,0 +1,16 @@
+import Category from "@/components/views/Admin/Category";
+import DashboardLayout from "@/components/layouts/DashboardLayout";
+
+const AdminCategoryPage = () => {
+  return (
+    <DashboardLayout
+      title="Category"
+      description="List of All Categories, Creating New Category, and Manage existing Categories"
+      type="admin"
+    >
+      <Category />
+    </DashboardLayout>
+  );
+};
+
+export default AdminCategoryPage;
